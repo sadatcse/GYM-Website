9c66ffe001b3c2820c37058eae8a0692c95c5a18
Minhajul Islam <mmminhaj221@gmail.com>
2024-06-14 14:22:43 +0600

    colorised navbar

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,7 +4,9 @@ import Navbar from "./components/Navbar";
 const Root = () => {
     return (
         <div>
-            <Navbar />
+            <div className="mx-auto w-[95%]">
+                <Navbar />
+            </div>
             {/* Dynamic section */}
             <div>
                 {/* <Outlet /> */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,42 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
 
-    const user = true;
+    const user = false;
 
     // Menu and route links are here
     const menu =
         <>
-            <NavLink to="/">Home</NavLink>
+            <NavLink className="text-red-600" to="/">Home</NavLink>
             <NavLink to="/">Trainers</NavLink>
             <NavLink to="/">News</NavLink>
             <NavLink to="/">Explore</NavLink>
             <NavLink to="/">About Us</NavLink>
             <NavLink to="/">Contact Us</NavLink>
         </>
+    const avatar = <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                    <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                    <a className="justify-between">
+                        Profile
+                        <span className="badge">New</span>
+                    </a>
+                </li>
+                <li><a>Settings</a></li>
+                <li><a>Logout</a></li>
+            </ul>
+        </div>
+    </div>
+
+    const buttons = <div className="">
+        <Link><button className="btn px-6 font-semibold text-white bg-red-600 rounded-md border hover:text-red-600 hover:bg-transparent  hover:border-red-600">Sign Up</button></Link>
+    </div>
 
     return (
         <div>
@@ -27,10 +50,10 @@ const Navbar = () => {
                             {menu}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">Multigym</a> 
+                    <a className="btn btn-ghost text-xl">Multigym</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 space-x-3">
+                    <ul className="menu menu-horizontal px-1 space-x-3 font-semibold text-sm flex items-center">
                         {menu}
                     </ul>
                 </div>
@@ -39,30 +62,12 @@ const Navbar = () => {
                     // If user exists then show avatar else sign up
                     user ?
                         <>
-                            <div className="navbar-end">
-                                <div className="dropdown dropdown-end">
-                                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                        <div className="w-10 rounded-full">
-                                            <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                                        </div>
-                                    </div>
-                                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li>
-                                            <a className="justify-between">
-                                                Profile
-                                                <span className="badge">New</span>
-                                            </a>
-                                        </li>
-                                        <li><a>Settings</a></li>
-                                        <li><a>Logout</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                            {avatar}
                         </>
                         :
                         <>
                             <div className="navbar-end">
-                                <a className="btn">Sign Up</a>
+                                {buttons}
                             </div>
                         </>
                 }
