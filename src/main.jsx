42f9819fdb35a71a1ed56653ab0578f02b55271d
sadat khan <sadatcse@gmail.com>
2024-06-29 20:14:24 +0600

    add error404 page

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {HelmetProvider } from 'react-helmet-async';
 import './index.css'
 import { router } from './routes/routes.jsx'
 import { RouterProvider } from 'react-router-dom'
@@ -10,8 +11,10 @@ AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <HelmetProvider>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
