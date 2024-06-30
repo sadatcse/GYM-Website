17385208556c4eecafc2b3928c98e4a1f4f6bfff
Minhajul Islam <mmminhaj221@gmail.com>
2024-06-30 17:17:40 +0600

    added couple membership

---
+++ src/components/About/CouplesMem.jsx
@@ -0,0 +1,53 @@
+const CouplesMem = () => {
+    const rowClass = "border text-center font-normal py-4"
+    const headClass = "border bg-gray-600 text-white font-semibold text-center text-base poppins"
+    return (
+        <div className="py-16">
+            <div className="screen">
+                {/* heading */}
+                <div className='mb-7 py-3 border border-gray-300'>
+                    <p className='text-center font-bold text-[#ebc270] text-3xl'>COUPLES MEMBERSHIP</p>
+                </div>
+
+                {/* table*/}
+                <div className="overflow-x-auto shadow">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th className={`${headClass}`}>DURATION</th>
+                                <th className={`${headClass}`}>REGULAR PRICE</th>
+                                <th className={`${headClass}`}>PROMO/INTRO PRICE</th>
+                            </tr>
+                        </thead>
+                        <tbody className='border'>
+                            {/* row 2 */}
+                            <tr>
+                                <th className={`${rowClass}`}>3 MONTH</th>
+                                <th className={`${rowClass}`}>BDT. 40000</th>
+                                <th className={`${rowClass}`}>BDT. 8K OFF 32000</th>
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th className={`${rowClass}`}>6 MONTH</th>
+                                <th className={`${rowClass}`}>BDT. 72000</th>
+                                <th className={`${rowClass}`}>BDT. 20K OFF 52000</th>
+                            </tr>
+                            {/* row 4 */}
+                            <tr>
+                                <th className={`${rowClass}`}>1 YEAR</th>
+                                <th className={`${rowClass}`}>BDT. 120000</th>
+                                <th className={`${rowClass}`}>BDT. 30K OFF 90000</th>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+
+                <p className="text-center font-semibold text-lg mt-5 text-gray-600">Note: Prices do not include VAT</p>
+            </div>
+        </div>
+    );
+};
+
+export default CouplesMem;
