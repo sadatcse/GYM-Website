24b0e3d814460fc083285245472ae817f560962b
Minhajul Islam <mmminhaj221@gmail.com>
2024-07-03 11:01:36 +0600

    fixed the theme on about us page , polished the navbar

--- src/components/About/AboutHero.jsx
+++ src/components/About/AboutHero.jsx
@@ -10,18 +10,17 @@ function AboutHero() {
         setShowModal(!showModal);
     };
     return (
-        <section className="  relative z-[1] pb-40 pt-8 bg-[#f4cb71] dark:bg-transparent">
+        <section className=" relative z-[1] pb-40 pt-8 bg-[#f4cb71]">
             <div className=" h-full absolute top-0 left-0 z-[-1] opacity-20" >
-
             </div>
             <div className="w-[90%] container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between items-center">
-                <div className="flex flex-col justify-center p-6 text-center rounded-sm xl:max-w-2xl lg:text-left items-center sm:items-start dark:text-white">
-                    <h1 className="text-5xl font-semibold leading-tight">Welcome to Multigym Premium <span className='text-[#d4000d] dark:text-gray-400'> where  </span> your transformation
+                <div className="flex flex-col justify-center p-6 text-center rounded-sm xl:max-w-2xl lg:text-left items-center sm:items-start ">
+                    <h1 className="text-5xl font-semibold leading-tight">Welcome to Multigym Premium <span className='text-[#d4000d] '> where  </span> your transformation
                     is our mission.
                     </h1>,  
                     <div className="max-w-md  pt-12 flex flex-col sm:flex-row items-center sm:justify-start sm:space-x-6">
-                        <div className=' shadow-lg border border-b-0 animate-bounce dark:border cursor-pointer p-4 flex items-center rounded-full'>
-                            <FaPlay className='text-2xl text-[#00ABE4] dark:text-white' onClick={toggleModal} />
+                        <div className=' shadow-lg border border-b-0 animate-bounce  cursor-pointer p-4 flex items-center rounded-full'>
+                            <FaPlay className='text-2xl text-[#00ABE4] ' onClick={toggleModal} />
                         </div>
                         <div className="flex flex-col justify-center items-center space-y-4 ">
                             <div>
