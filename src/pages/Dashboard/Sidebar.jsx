4b207af4cbd13f5a29245defe0ad1d6d79d44f85
Minhajul Islam <mmminhaj221@gmail.com>
2024-07-05 14:53:51 +0600

    upated dashboard icons

--- src/pages/Dashboard/Sidebar.jsx
+++ src/pages/Dashboard/Sidebar.jsx
@@ -6,6 +6,16 @@ import { AuthContext } from '../../providers/AuthProvider';
 import Swal from 'sweetalert2';
 import logo from "../../assets/logo.png"
 // import logo from "../../assets/gymlogo.jpg"
+import { RiListCheck2 } from "react-icons/ri";
+import { IoPersonAddSharp } from "react-icons/io5";
+import { BsPlusCircleDotted } from "react-icons/bs";
+import { VscNewFile } from "react-icons/vsc";
+import { AiOutlineTeam } from "react-icons/ai";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { BiMessageSquareAdd } from "react-icons/bi";
+import { LuMessagesSquare } from "react-icons/lu";
+
+
 
 const Sidebar = () => {
 
@@ -33,48 +43,48 @@ const Sidebar = () => {
     };
 
     return (
-        <div   className="border-r right-inner-shadow border-gray-400 border-dashed h-screen poppins text-[#737373] max-w-[200px]">
-            {/* <img src={logo} alt="multygym premium" className='w-16' /> */}
+        <div className="border-r right-inner-shadow border-gray-400 border-dashed h-screen poppins text-[#737373] max-w-[200px]">
+            <img src={logo} alt="multygym premium" className='w-16 ml-5 py-7' />
             <div className=''>
-                <p className='text-lg font-semibold mx-5'>Logo</p>
+                {/* <p className='text-lg font-semibold mx-5'>Logo</p> */}
                 <nav className=''>
                     <ul>
-                        <li className="text-sm text-left mt-3  mx-5">Blogs</li>
+                        <li className="text-sm text-left mt-3   mx-5">Blogs</li>
                         <li className="text-left mt-3  mx-5">
                             <Link to="/blogs/view" className="flex gap-3 items-center">
-                                <FaBlog className="" />
+                                <RiListCheck2 />
                                 List
                             </Link>
                         </li>
                         <li className="text-left mt-3  mx-5">
                             <Link to="/blogs/create" className="flex gap-3 items-center">
-                                <FaBlog className="" />
+                                <VscNewFile />
                                 Create
                             </Link>
                         </li>
                         <li className="text-sm text-left mt-3  mx-5">Testimonial</li>
                         <li className="text-left mt-3  mx-5">
                             <Link to="/testimonials/view" className="flex gap-3 items-center">
-                                <FaComments className="" />
+                                <LuMessagesSquare />
                                 List
                             </Link>
                         </li>
                         <li className="text-left mt-3  mx-5">
                             <Link to="/testimonials/create" className="flex gap-3 items-center">
-                                <FaComments className="" />
+                                <BiMessageSquareAdd />
                                 Create
                             </Link>
                         </li>
                         <li className="text-sm text-left mt-3  mx-5">Teams</li>
                         <li className="text-left mt-3  mx-5">
                             <Link to="/teams/view" className="flex gap-3 items-center">
-                                <FaUsers className="" />
+                                <AiOutlineTeam />
                                 List
                             </Link>
                         </li>
                         <li className="text-left mt-3  mx-5">
                             <Link to="/teams/add" className="flex gap-3 items-center">
-                                <FaUsers className="" />
+                                <AiOutlineUserAdd />
                                 Add
                             </Link>
                         </li>
