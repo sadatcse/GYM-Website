131fab2433705656337a4115500600ab5fc28931
Minhajul Islam <mmminhaj221@gmail.com>
2024-07-13 12:10:37 +0600

    fixed jwt issue in client site

--- src/Hook/UseAxioSecure.jsx
+++ src/Hook/UseAxioSecure.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 export const axiosSecure = axios.create({
-    baseURL: 'https:/multigym-premium-server.vercel.app/'
+    // baseURL: 'https:/multigym-premium-server.vercel.app/'
+    baseURL: 'https://multi-gym-server-vwyee.ondigitalocean.app/'
 })
 const UseAxioSecure = () => {
+    axiosSecure.defaults.withCredentials = true ;
     return axiosSecure;
 
 };
