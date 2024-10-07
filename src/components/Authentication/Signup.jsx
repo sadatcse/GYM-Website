e3bbd9f228d39432d24e7f88963c6d45fe9efbcf
Minhajul Islam <mmminhaj221@gmail.com>
2024-10-07 15:40:44 +0600

    added validation on signup page

--- src/components/Authentication/Signup.jsx
+++ src/components/Authentication/Signup.jsx
@@ -1,16 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import MemberRegisterInput from './components/MemberRegisterInput';
 import MemberRegisterSelect from './components/MemberRegisterSelect';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+// Validation Schema using Yup
+const validationSchema = yup.object().shape({
+    full_name: yup
+        .string()
+        .required('Full Name is required')
+        .matches(/^[a-zA-Z\s]+$/, 'Full Name must be only letters')
+        .min(4, 'Full Name must be at least 4 words'),
+    contact_no: yup
+        .string()
+        .required('Contact Number is required')
+        .matches(/^[0-9]+$/, 'Contact Number must be numeric')
+        .min(11, 'Contact Number must be at least 11 digits'),
+    nid_number: yup
+        .string()
+        .required('NID is required')
+        .matches(/^[0-9]+$/, 'NID must be numeric')
+        .min(10, 'NID must be at least 10 digits'),
+    address: yup
+        .string()
+        .required('Address is required')
+        // .matches(/^[a-zA-Z\s]+$/, 'Address cannot contain numbers')
+        .min(5, 'Address must be at least 5 words'),
+    email: yup
+        .string()
+        .email('Invalid email format')
+        .required('Email is required')
+        .matches(
+            /^(?=.*@(gmail|yahoo|hotmail|outlook)\.com$)/,
+            'Use a valid email adress'
+        ),
+    emergency_contact_number: yup
+        .string()
+        .matches(/^[0-9]+$/, 'Emergency Contact must be numeric')
+        .min(10, 'Emergency Contact Number must be at least 10 digits'),
+    fb_id: yup
+        .string()
+        .matches(/^[a-zA-Z0-9]+$/, 'FB ID must not contain spaces')
+        .nullable(),
+    profession: yup
+        .string()
+        .matches(/^[a-zA-Z\s]+$/, 'Profession cannot contain numbers')
+        .required('Profession is required'),
+    weight: yup
+        .number()
+        .typeError('Weight must be a number')
+        .max(700, 'Weight cannot exceed 700 kg'),
+});
 
 const Signup = () => {
-    const { register, handleSubmit, formState: { errors }, watch } = useForm();
+    const { register, handleSubmit, formState: { errors }, watch } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
     const [age, setAge] = useState('');
-    const [memberTypes] = useState(['Monthly', 'Weekly', 'Daily', "Package"]);
+    const [memberTypes] = useState(['Monthly', 'Weekly', 'Daily', 'Package']);
     const [bloodGroups] = useState(['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-']);
-
-    // Watch date of birth to calculate age
+    const navigate = useNavigate();
     const dateOfBirth = watch('date_of_birth');
 
     useEffect(() => {
@@ -25,41 +79,34 @@ const Signup = () => {
         }
     }, [dateOfBirth]);
 
-    // Function to handle form submission
     const onSubmit = (data) => {
-        console.log(data)
         const signUpUser = async (userData) => {
             try {
-                console.log(userData)
                 const response = await axios.post('https://multigym-management-server-dmmji.ondigitalocean.app/api/users/signup', userData);
-                alert("done")
-                console.log('User created successfully:', response.data);
+                toast.success('User created successfully');
+                navigate("/", { replace: true });
             } catch (error) {
-                // Handle any errors
-                alert("error")
-                console.log(error)
-                console.error('Error signing up the user:', error.response ? error.response.data : error.message);
+                toast.error(error.response ? error.response.data : 'An error occurred');
             }
         };
         signUpUser(data);
-
     };
+
     return (
         <article className="w-full md:p-8 flex items-center justify-center ">
-            <form className="w-full max-w-4xl border  rounded-xl py-6 px-6  shadow" onSubmit={handleSubmit(onSubmit)}>
+            <form className="w-full max-w-4xl border rounded-xl py-6 px-6 shadow" onSubmit={handleSubmit(onSubmit)}>
                 <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-                    {/* Personal Information Section */}
-                    <p className="text-2xl  text-center px-4 py-2 rounded-xl font-semibold my-4 mt-2 lg:col-span-3  md:col-span-2">
+                    <p className="text-2xl text-center px-4 py-2 rounded-xl font-semibold my-4 mt-2 lg:col-span-3 md:col-span-2">
                         Sign Up Now
                     </p>
 
                     <MemberRegisterInput
                         type="text"
+                        label="Full Name"
                         register={register}
                         error={errors}
                         name="full_name"
                         isRequired={true}
-                        label="Full Name"
                     />
 
                     <MemberRegisterSelect
@@ -88,16 +135,16 @@ const Signup = () => {
 
                     <MemberRegisterInput
                         type="text"
+                        label="Nickname"
                         register={register}
                         error={errors}
                         name="nickname"
                         isRequired={false}
-                        label="Nickname"
                     />
 
                     <MemberRegisterInput
                         type="date"
-                        label="Date Of Birth "
+                        label="Date of Birth"
                         register={register}
                         error={errors}
                         name="date_of_birth"
@@ -137,7 +184,7 @@ const Signup = () => {
                     </MemberRegisterSelect>
 
                     <MemberRegisterSelect
-                        label="Gender "
+                        label="Gender"
                         register={register}
                         error={errors}
                         name="gender"
@@ -200,7 +247,7 @@ const Signup = () => {
                     />
 
                     <MemberRegisterSelect
-                        label="Blood Group "
+                        label="Blood Group"
                         register={register}
                         error={errors}
                         name="blood_group"
@@ -214,15 +261,6 @@ const Signup = () => {
                         ))}
                     </MemberRegisterSelect>
 
-                    {/* <MemberRegisterInput
-                        type="text"
-                        label="Age"
-                        value={age || ""}
-                        isRequired={false}
-                        readOnly={true}
-                    /> */}
-
-                    {/* Height in Feet */}
                     <MemberRegisterSelect
                         label="Height"
                         register={register}
@@ -232,24 +270,13 @@ const Signup = () => {
                     >
                         <option value="">Select Height</option>
                         {Array.from({ length: 97 }, (_, i) => {
-                            if (i < 36) {
-                                const feet = 3; // start from 3 feet
-                                const inches = i; // increments of inches
-                                return (
-                                    <option key={i} value={`${feet} feet ${inches}`}>
-                                        {feet} feet {inches}
-                                    </option>
-                                );
-                            } else if (i >= 36 && i <= 95) {
-                                const feet = Math.floor(i / 12) + 3; // convert to feet starting from 3
-                                const inches = i % 12;
-                                return (
-                                    <option key={i} value={`${feet} feet ${inches}`}>
-                                        {feet} feet {inches}
-                                    </option>
-                                );
-                            }
-                            return null;
+                            const feet = Math.floor(i / 12) + 3;
+                            const inches = i % 12;
+                            return (
+                                <option key={i} value={`${feet} feet ${inches} inches`}>
+                                    {feet} feet {inches} inches
+                                </option>
+                            );
                         })}
                     </MemberRegisterSelect>
 
@@ -262,7 +289,6 @@ const Signup = () => {
                         isRequired={false}
                     />
 
-                    {/* Profession as a text input */}
                     <MemberRegisterInput
                         type="text"
                         label="Profession"
@@ -280,6 +306,7 @@ const Signup = () => {
                     Sign up
                 </button>
             </form>
+            <ToastContainer />
         </article>
     );
 };
